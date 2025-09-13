2119cc504857cad237bfe92ddb7e8915c2569e3f
ALPHA7105 <ssworld7105@gmail.com>
2025-09-13 15:47:13 +0400

    Update Discount Calculator.py

--- Discount Calculator.py
+++ Discount Calculator.py
@@ -1,1 +1,11 @@
-
+n=float(input("Enter price of product"))
+if n>=200 and n<500:
+  a=n-(n*0.1)
+elif n>=500 and n<1000:
+  a=n-(n*0.3)
+elif n>=1000:
+  a=n-(n*0.5)
+else:
+  a=n
+  print("You are not eligible for a discount")
+print("Total amount you have to pay is ",a)
